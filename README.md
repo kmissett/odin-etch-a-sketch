bc29297c08bed116d25cb18dfce4f6a62bc52247
kmissett <86633265+kmissett@users.noreply.github.com>
2022-03-01 21:10:44 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # odin-etch-a-sketch
+
+Another Odin Project. The Etch-A-Sketch clears with a button, and we can change the resolution (number of rows) with a number input.
+
+TODOS: 
+1. basic CSS for the number input and the button
+2. get the Etch-A-Sketch to shake when we clear the screen
+3. get the knobs to work with arrow keys (plus hit collision on the sides)
+4. maybe try to put the pen up and down on clicking the screen, or pressing some key
+5. introduce color, as the project notes suggested
---
+++ script.js
@@ -0,0 +1,68 @@
+const etchASketch = document.querySelector(".etch-a-sketch")
+const screen = document.querySelector(".screen")
+const screenHeight = 24
+const aspectRatio = 1.5
+const screenWidth = screenHeight * aspectRatio
+
+screen.style.height = screenHeight + "rem"      // 24rem = 384px
+screen.style.width = screenWidth + "rem"        // 36rem = 576px
+
+let pixels = []
+
+
+const drawPixels = (resolutionValue) => {
+
+    const rows = (resolutionValue) ? resolutionValue : 16       // default 16
+    const columns = aspectRatio * rows                          // default 24
+    const pixelSize = screenHeight / rows                       // 24rem / 16 = 1.5rem
+    
+    for(let i=0; i < rows; i++) {
+        for (let j=0; j < columns; j++) {
+            const pixel = document.createElement("div")
+            pixel.setAttribute("data-row", i)
+            pixel.setAttribute("data-column", j)
+            pixel.style.width = pixelSize + "rem"
+            pixel.style.height = pixelSize + "rem"
+            pixel.style.backgroundColor = "black"
+            pixel.classList.add("pixel")
+            screen.appendChild(pixel)
+        }
+    }
+    pixels = document.querySelectorAll(".pixel")
+    pixels.forEach(pixel => pixel.addEventListener("mouseenter", darkenPixel))
+}
+
+const removePixels = () => {
+    while (screen.hasChildNodes()) screen.removeChild(screen.childNodes[0])
+}
+
+const darkenPixel = (e) => {
+    const opacity = parseFloat(getComputedStyle(e.target).getPropertyValue("--opacity"))
+    e.target.style.setProperty("--opacity", opacity + 0.25)
+}
+
+const resolutionForm = document.getElementById("resolution-form")
+resolutionForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+    removePixels()
+    const newResolutionValue = parseInt(resolution.value)
+    drawPixels(newResolutionValue)
+    resolution.value = newResolutionValue
+})
+
+
+const clearButton = document.querySelector(".clear")
+clearButton.addEventListener("click", () => {
+        removePixels()
+        const resolutionValue = resolution.value ? resolution.value : 16
+        drawPixels(parseInt(resolutionValue))
+        resolution.value = resolutionValue
+})
+
+
+
+
+
+
+drawPixels()
+
